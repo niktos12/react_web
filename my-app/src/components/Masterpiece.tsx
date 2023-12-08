00db4b000e4fab8babe7c2ecd57c93508c2a4885
niktos12 <nikitosss121207@gmail.com>
2023-12-08 18:11:41 +0400

    ....

--- my-app/src/components/Masterpiece.tsx
+++ my-app/src/components/Masterpiece.tsx
@@ -1,5 +1,7 @@
 import { arrowRightImg } from "./Categories"
 import { MySlider } from "./Slider"
+import  SimpleSlider  from "./SimpleSlider"
+import Slider from "react-slick"
 export function Masterpiece(){
     return (
         <div className="flex flex-col bg-gray-100 p-20">
@@ -7,8 +9,8 @@ export function Masterpiece(){
                 <h1 className="font-bold text-4xl">Latest masterpieces</h1>
                 <a className="font-medium text-base flex flex-row gap-4 items-center text-blue-600">Discover More Masterpieces<img src={arrowRightImg}></img></a>
             </div>
-            <div className="flex flex-row px-20">
-                <MySlider />
+            <div className="flex flex-row px-20 justify-center items-center w-4/5 mx-auto">
+                <SimpleSlider />
             </div>
         </div>
     )
--- my-app/src/components/SimpleSlider.tsx
+++ my-app/src/components/SimpleSlider.tsx
@@ -3,7 +3,7 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-import Swiper from 'swiper';
+import Swiper from 'react-id-swiper';
 import flowerFirst from "../img/flower-1.png"
 import flowerSecond from "../img/flower-2.png"
 import flowerThird from "../img/flower-3.png"
@@ -15,10 +15,11 @@ import userThird from "../img/user_3.png"
 import userFourth from "../img/user_4.png"
 import userFifth from "../img/user_5.png"
 import navArrow from "../img/chevron-right.png"
-export function MySlider()  {
-  const swiper = new Swiper('.swiper', {
-    slidesPerView: 4,
-    spaceBetween: 50,
+class SimpleSwiper extends React.Component {
+    render() {
+      const params = {
+        slidesPerView: 3.5,
+        spaceBetween: 50,
     loop: true,
     grabCursor: true,
     breakpoints: {
@@ -33,23 +34,23 @@ export function MySlider()  {
     navigation: {
       nextEl: '#next',
     },
-  })
- 
+      }
   
       return (
-        <div className="swiper flex flex-row justify-center items-center max-w-7xl">
-        <div className="swiper-wrapper flex flex-row justify-center items-center ">
-          <div className="swiper-slide bg-white w-72 rounded-lg flex flex-col">
-            <img src={flowerFirst} className="rounded-t-lg"></img>
+        <div className="flex flex-row justify-center items-center max-w-7xl box-border">
+        <Swiper {...params} slideClass="w-72" containerClass="w-full h-96 justify-center items-center" 
+        wrapperClass="w-full h-96 justify-center items-center flex flex-row">
+          <div className="bg-white max-w-72 rounded-lg flex flex-col box-border">
+          <img src={flowerFirst} className="rounded-t-lg"></img>
                <div className="flex flex-row p-4 items-start gap-2">
                  <img src={userFirst}></img>
                <div className="flex flex-col">
                    <p className="text-gray-900">Flower Decorations</p>
                    <p className="text-sm text-gray-600">by Melvina Spring</p>
                </div>
-             </div></div>
-          
-          <div className="swiper-slide bg-white w-72 rounded-lg flex flex-col box-border">
+             </div>
+          </div>
+          <div className="bg-white max-w-72 rounded-lg flex flex-col box-border">
           <img src={flowerSecond} className="rounded-t-lg"></img>
                <div className="flex flex-row p-4 items-start gap-2">
                  <img src={userSecond}></img>
@@ -59,7 +60,7 @@ export function MySlider()  {
                </div>
              </div>
           </div>
-          <div className="swiper-slide bg-white w-72 rounded-lg flex flex-col box-border">
+          <div className="bg-white max-w-72 rounded-lg flex flex-col box-border">
           <img src={flowerThird} className="rounded-t-lg"></img>
                <div className="flex flex-row p-4 items-start gap-2">
                  <img src={userThird}></img>
@@ -69,7 +70,7 @@ export function MySlider()  {
                </div>
              </div>
           </div>
-          <div className="swiper-slide bg-white w-72 rounded-lg flex flex-col box-border">
+          <div className="bg-white max-w-72 rounded-lg flex flex-col box-border">
           <img src={flowerFourth} className="rounded-t-lg"></img>
                <div className="flex flex-row p-4 items-start gap-2">
                  <img src={userFourth}></img>
@@ -79,8 +80,8 @@ export function MySlider()  {
                </div>
              </div>
           </div>
-          <div className="swiper-slide bg-white w-72 rounded-lg flex flex-col box-border">
-          <img src={flowerFifth} className="rounded-t-lg"></img>
+          <div className="bg-white max-w-72 rounded-lg flex flex-col box-border">
+          <img src={flowerFifth} className="rounded-t-lg h-40"></img>
                <div className="flex flex-row p-4 items-start gap-2">
                  <img src={userFifth}></img>
                <div className="flex flex-col">
@@ -89,9 +90,9 @@ export function MySlider()  {
                </div>
              </div>
           </div>
+        </Swiper>
         </div>
-        <div id="next"><img src={navArrow}></img></div>
-        </div>
-      );
+      )
     }
-  
+  }
+export default SimpleSwiper
